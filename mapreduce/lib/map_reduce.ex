8175c4b7e06de7f903aa2a5cb24e2beac762e44d
Changfeng Lou <louchangfeng@outlook.com>
2023-07-23 00:05:30 +0800

    Add single thread `map` and `reduce`

--- mapreduce/lib/map_reduce.ex
+++ mapreduce/lib/map_reduce.ex
@@ -3,7 +3,32 @@ defmodule MapReduce do
   Documentation for `MapReduce`.
   """
 
-  def hello do
-    :world
+  def map(s) do
+    s
+    |> String.split([" ", "\n", "\t", ","])
+    |> Enum.filter(fn w -> String.length(w) != 0 end)
+    |> Enum.map(fn w -> {w, 1} end)
+  end
+
+  def reduce(list) do
+    list
+    |> get_keys()
+    |> Enum.map(fn k -> reduce_by_key(k, list) end)
+  end
+
+  defp get_keys(list) do
+    list
+    |> Enum.map(fn {k, _v} -> k end)
+    |> Enum.sort()
+    |> Enum.dedup()
+  end
+
+  defp reduce_by_key(key, list) do
+    sum =
+      list
+      |> Enum.filter(fn {k, _v} -> k == key end)
+      |> List.foldl(0, fn {_k, v}, acc -> acc + v end)
+
+    {key, sum}
   end
 end
