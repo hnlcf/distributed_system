ae71557d3eae9445519a2740e019ee8f4038fc22
Changfeng <louchangfeng@outlook.com>
2023-07-24 11:14:40 +0800

    test: make test past

--- mapreduce/test/map_reduce_test.exs
+++ mapreduce/test/map_reduce_test.exs
@@ -3,6 +3,12 @@ defmodule MapReduceTest do
   doctest MapReduce
 
   test "greets the world" do
-    assert MapReduce.hello() == :world
+    assert MapReduce.map("this is my test case") == [
+             {"this", 1},
+             {"is", 1},
+             {"my", 1},
+             {"test", 1},
+             {"case", 1}
+           ]
   end
 end
